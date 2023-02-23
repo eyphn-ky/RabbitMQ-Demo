4a4453b72ebf13ee8ca10394ca275a82c701ffec
eyphn-ky <eyuphan.kaya34@gmail.com>
2023-02-23 19:26:10 +0300

    First look at rabbitmq

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://oxystewb:g8ZxUdsUXLX2FY6u0PfNIRftYF7dc2HC@moose.rmq.cloudamqp.com/oxystewb");
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel= connection.CreateModel();
+
+//Queue oluşturma(okunacak kuyruk)
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Queue’dan mesaj okuma
+//mesaj okuyabilmek için bir event oluşturulmalı
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue:"example-queue",false,consumer);//bildirilen kuyruğa bir mesaj geldiğinde onu almak için bu şekilde kullanırız.
+                                                           //autoack => mesaj alındığında silinsin mi
+consumer.Received += (sender, e) =>
+{
+    //Kuyruğa gelen mesajların işlendiği yerdir.
+    //e.Body:Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+    //e.Body.Span veya e.Body.ToArray():Kuyruktaki mesajın byte verisini getirecektir. 
+
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+Console.Read();
---
+++ Publisher/Publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+ConnectionFactory factory = new ConnectionFactory();
+//bağlantı oluşturma
+factory.Uri = new("amqps://oxystewb:g8ZxUdsUXLX2FY6u0PfNIRftYF7dc2HC@moose.rmq.cloudamqp.com/oxystewb");
+
+//bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//queue oluşturma
+channel.QueueDeclare(queue:"example-queue",exclusive:false);//durable => kuyruktaki mesajların kalıcığıyla alakalı
+                                                            //exlusive => kuyruk özel mi değil mi? Bu kuyrukta birden fazla bağlantıyla bu kuyrukta işlem yapıp yapamayacağımızı belirler.
+                                                            //exclusive bu proje için false olmalıdır aynı hem consumer hem publisher bu kuyruğa bağlanabilir olmalıdır.
+                                                            //autoDelete=> kuyruğun içindeki tüm mesajlar tükedildikten sonra kuyruk silinsin mi silinmesin mi onu belirler.
+
+
+//queue'ya mesaj gönderme. 
+//RabbitMQ kuyruğa atacağı verileri byte türünden kabul etmektedir. Haliyle mesajları bizim byte türüne dönüştürmemiz gerekecektir.
+
+//Tek bir mesaj göndermek için deneme
+//byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+//channel.BasicPublish(exchange:"",routingKey:"example-queue",body:message);//boş bırakmak veya parametre vermemek defaul exchange olan direct exchange ile çalışmasını sağlayacaktır.
+
+//Biraz da oyun oynayalım
+for(int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);//boş bırakmak veya parametre vermemek defaul exchange olan direct exchange ile çalışmasını sağlayacaktır.
+}
+
+
+
+//git bak bakalım rabbit mq managerde oluşturmuş mu
+Console.Read();
