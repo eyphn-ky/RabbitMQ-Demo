fc2ac545f66f7ffc3ec945c28f4b67a6a782c776
eyphn-ky <eyuphan.kaya34@gmail.com>
2023-02-24 20:15:56 +0300

    set an example in direct exchange

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -2,29 +2,50 @@
 using RabbitMQ.Client.Events;
 using System.Text;
 
-//Bağlantı oluşturma
+//1-Bağlantı oluşturma
 ConnectionFactory factory = new();
 factory.Uri = new("amqps://oxystewb:g8ZxUdsUXLX2FY6u0PfNIRftYF7dc2HC@moose.rmq.cloudamqp.com/oxystewb");
 
-//Bağlantı aktifleştirme ve kanal açma
+//2-Bağlantı aktifleştirme ve kanal açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel= connection.CreateModel();
 
-//Queue oluşturma(okunacak kuyruk)
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+//3-Queue oluşturma(okunacak kuyruk)
+//channel.QueueDeclare(queue: "example-queue", exclusive: false);
 
-//Queue’dan mesaj okuma
+//4-Queue’dan mesaj okuma
 //mesaj okuyabilmek için bir event oluşturulmalı
+//EventingBasicConsumer consumer = new(channel);
+//channel.BasicConsume(queue:"example-queue",autoAck:false,consumer);//bildirilen kuyruğa bir mesaj geldiğinde onu almak için bu şekilde kullanırız.
+//channel.BasicQos(0,1,false);                                                               //autoack => mesaj başarılı veya başarısız olarak alındığında silinsin mi? 
+//consumer.Received += (sender, e) =>
+//{
+//    //Kuyruğa gelen mesajların işlendiği yerdir.
+//    //e.Body:Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
+//    //e.Body.Span veya e.Body.ToArray():Kuyruktaki mesajın byte verisini getirecektir. 
+
+//    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+//    channel.BasicAck(e.DeliveryTag,multiple:false); //message acknowledgement configured
+//};
+
+
+//Direct Exchange Deneme Başlangıcı//
+//1-Publisher'da ki channel ile aynı isim ve türde exchange tanımlanmalıdır.
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);//publisher'da ki exchange ile birebir aynı isim ve type'a sahip bir exchange tanımlanmalıdır.
+
+//2-Publisher tarafından rounting key'de bulunan değerdeki kuyruğa gönderilen mesajları, kendi oluşturduğumuz kuyruğa yönlendirerek tüketmemiz gerekmektedir. Bunun için öncelikle bir kuyruk oluşturulmalıdır.
+string queueName = channel.QueueDeclare().QueueName;
+
+//3-publisher hangi kuyruğa attıysa oraya bağlan.
+channel.QueueBind(queue: queueName, exchange: "direct-exchange-example",routingKey:"direct-queue-example");
+
+//4-mesajları tüket
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue:"example-queue",autoAck:false,consumer);//bildirilen kuyruğa bir mesaj geldiğinde onu almak için bu şekilde kullanırız.
-channel.BasicQos(0,1,false);                                                               //autoack => mesaj başarılı veya başarısız olarak alındığında silinsin mi? 
+channel.BasicConsume(queue:queueName,autoAck:true,consumer:consumer);
 consumer.Received += (sender, e) =>
 {
-    //Kuyruğa gelen mesajların işlendiği yerdir.
-    //e.Body:Kuyruktaki mesajın verisini bütünsel olarak getirecektir.
-    //e.Body.Span veya e.Body.ToArray():Kuyruktaki mesajın byte verisini getirecektir. 
-
-    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
-    channel.BasicAck(e.DeliveryTag,multiple:false); //message acknowledgement configured
+    string message = Encoding.UTF8.GetString(e.Body.Span);
+    Console.WriteLine(message);
 };
+//Direct Exchange Deneme Bitişi//
 Console.Read();
--- Publisher/Publisher/Program.cs
+++ Publisher/Publisher/Program.cs
@@ -1,38 +1,50 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
 ConnectionFactory factory = new ConnectionFactory();
-//bağlantı oluşturma
+//1-bağlantı oluşturma
 factory.Uri = new("amqps://oxystewb:g8ZxUdsUXLX2FY6u0PfNIRftYF7dc2HC@moose.rmq.cloudamqp.com/oxystewb");
 
-//bağlantı aktifleştirme ve kanal açma
+//2-bağlantı aktifleştirme ve kanal açma
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-//queue oluşturma
-channel.QueueDeclare(queue:"example-queue",exclusive:false,durable:true);//durable => kuyruktaki mesajların kalıcığıyla alakalı
-                                                            //exlusive => kuyruk özel mi değil mi? Bu kuyrukta birden fazla bağlantıyla bu kuyrukta işlem yapıp yapamayacağımızı belirler.
-                                                            //exclusive bu proje için false olmalıdır aynı hem consumer hem publisher bu kuyruğa bağlanabilir olmalıdır.
-                                                            //autoDelete=> kuyruğun içindeki tüm mesajlar tükedildikten sonra kuyruk silinsin mi silinmesin mi onu belirler.
+//3-queue oluşturma
+//channel.QueueDeclare(queue:"example-queue",exclusive:false,durable:true);//durable => kuyruktaki mesajların kalıcığıyla alakalı
+//                                                                         //exlusive => kuyruk özel mi değil mi? Bu kuyrukta birden fazla bağlantıyla bu kuyrukta işlem yapıp yapamayacağımızı belirler.
+//                                                                         //exclusive bu proje için false olmalıdır aynı hem consumer hem publisher bu kuyruğa bağlanabilir olmalıdır.
+//                                                                         //autoDelete=> kuyruğun içindeki tüm mesajlar tükedildikten sonra kuyruk silinsin mi silinmesin mi onu belirler.
 
 
-//queue'ya mesaj gönderme. 
+//4-queue'ya mesaj gönderme. 
 //RabbitMQ kuyruğa atacağı verileri byte türünden kabul etmektedir. Haliyle mesajları bizim byte türüne dönüştürmemiz gerekecektir.
 
 //Tek bir mesaj göndermek için deneme
 //byte[] message = Encoding.UTF8.GetBytes("Merhaba");
 //channel.BasicPublish(exchange:"",routingKey:"example-queue",body:message);//boş bırakmak veya parametre vermemek defaul exchange olan direct exchange ile çalışmasını sağlayacaktır.
 
+
 //Biraz da oyun oynayalım
-IBasicProperties properties=channel.CreateBasicProperties();
-properties.Persistent = true;
-for(int i = 0; i < 100; i++)
+//IBasicProperties properties=channel.CreateBasicProperties();
+//properties.Persistent = true;
+//for(int i = 0; i < 100; i++)
+//{
+//    await Task.Delay(200);
+//    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i);
+//    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message,basicProperties:properties);//boş bırakmak veya parametre vermemek defaul exchange olan direct exchange ile çalışmasını sağlayacaktır.
+//}
+
+//Direct Exchange Deneme Başlangıcı//
+channel.ExchangeDeclare(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+while (true)
 {
-    await Task.Delay(200);
-    byte[] message = Encoding.UTF8.GetBytes("Merhaba"+i);
-    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message,basicProperties:properties);//boş bırakmak veya parametre vermemek defaul exchange olan direct exchange ile çalışmasını sağlayacaktır.
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(
+        exchange: "direct-exchange-example",
+        routingKey:"direct-queue-example",
+        body:byteMessage);
 }
+//Direct Exchange Deneme Bitişi//
 
-
-
-//git bak bakalım rabbit mq managerde oluşturmuş mu
 Console.Read();
